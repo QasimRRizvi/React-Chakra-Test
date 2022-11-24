60ed531abb2c85dbd1ad79a7dc6173b307e3a10a
QasimRRizvi <qaism@geeksofkolcahi.com>
2022-11-24 11:51:54 +0500

    Use Home in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
-import {
-  ChakraProvider,
-  Box,
-  Grid,
-  theme,
-  HStack,
-} from "@chakra-ui/react";
+import { ChakraProvider, Box, Grid, theme } from "@chakra-ui/react";
 import { ColorModeSwitcher } from "./ColorModeSwitcher";
-import { NFTModal } from "./components/Modals/nft";
-import { NFT_DETAILS } from "./resources/data/nftDetails";
+import { AppProvider } from "./Context/provider";
+import { Home } from "./Pages/Home";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <Box fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <HStack spacing={8}>
-          {NFT_DETAILS.map((details) => (
-            <NFTModal data={details} />
-          ))}
-        </HStack>
-      </Grid>
-    </Box>
+    <AppProvider>
+      <Box fontSize="xl">
+        <Grid minH="100vh" p={3}>
+          <ColorModeSwitcher justifySelf="flex-end" />
+          <Home />
+        </Grid>
+      </Box>
+    </AppProvider>
   </ChakraProvider>
 );
