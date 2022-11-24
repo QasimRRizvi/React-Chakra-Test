a46e6bdeae4d4b8a913b8d2f43cd5f1103faaf69
QasimRRizvi <qaism@geeksofkolcahi.com>
2022-11-24 14:23:31 +0500

    Update CountDown Component

--- src/components/CountDown/index.tsx
+++ src/components/CountDown/index.tsx
@@ -1,21 +1,20 @@
+import { Text } from '@chakra-ui/react';
 import Countdown, { CountdownRendererFn } from 'react-countdown';
+import { addStartPad } from '../../Utils/common';
 
 interface props {
   startDate: Date;
 }
 
 export const CountDown: React.FC<props> = ({ startDate }) => {
-  const addStartPad = (num: number) => {
-    return num.toString().padStart(2, '0');
-  };
-  const renderer: CountdownRendererFn = ({ days, hours, minutes, seconds }) => {
-    return (
-      <span>
-        {addStartPad(days)}:{addStartPad(hours)}:{addStartPad(minutes)}:
-        {addStartPad(seconds)}s
-      </span>
-    );
-  };
+  const renderer: CountdownRendererFn = ({ days, hours, minutes, seconds }) => (
+    <Text fontWeight='bold'>
+      {`
+      ${addStartPad(days)} : ${addStartPad(hours)} : ${addStartPad(
+        minutes
+      )} : ${addStartPad(seconds)}s`}
+    </Text>
+  );
   return (
     <Countdown zeroPadDays={3} date={new Date(startDate)} renderer={renderer} />
   );
